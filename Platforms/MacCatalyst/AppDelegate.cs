1f8df40a643e336ad4642e62db1ecaa98b679211
Prathyush Taneti <prathyush@iqdynamics-tech.com>
2024-02-05 10:03:16 +0530

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Neptune
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
